8757b149038413496ae5a4b4fd846a1d8a723c65
Elliana May <me@mause.me>
2021-08-09 00:13:15 +0800

    Fix endpoint

--- api/dump.ts
+++ api/dump.ts
@@ -5,6 +5,6 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
 
-export default async function(req: VercelRequest, res: VercelResponse {
-    return Object.keys(process.env);
+export default async function(req: VercelRequest, res: VercelResponse) {
+    res.json({keys: Object.keys(process.env), supabase});
 }
