c359aa0bb67d8b40a04c3196b0a0737572761411
Elliana May <me@mause.me>
2021-08-09 14:36:24 +0800

    Vendors?

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import "./App.css";
 import { definitions } from "./supabase";
 import { useFilter, useTable } from "react-supabase-fp";
 import { pipe, constant } from "fp-ts/function";
+import { None } from 'fp-ts/Option';
 import * as RD from "@devexperts/remote-data-ts";
 import useSWR from "swr";
 
 function App() {
   const result = useTable<definitions["Bill"]>("Bill", "*");
   const { data, error } = useSWR("https://launtel.vercel.app/api/transactions");
+  
+  const vendors = pipe(useTable<definitions['Vendor']>('Vendor', '*'), RD.fold3(constant(None), e => None, s => s));
 
   return (
     <div className="App">
@@ -27,7 +30,7 @@ function App() {
                   {result.map((row) => (
                     <div key={row.id}>
                       <h2>
-                        ${row.amount} — {row.vendor.name}
+                        ${row.amount} — {vendors[row.vendor].name}
                       </h2>
                     </div>
                   ))}
