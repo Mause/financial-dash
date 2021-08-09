d9de847a0d080454bbe7448b640086c47c2fb43b
Elliana May <me@mause.me>
2021-08-09 12:35:55 +0800

    Call supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { definitions } from './supabase';
+import { useFilter, useTable } from 'react-supabase-fp';
 
 function App() {
+  const filter = useFilter<definitions['Bill']>(query =>
+    query.contains('text', 'production')
+  );
+  const result = useTable<definitions['Bill']>('Bill', '*', filter);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          {
+            pipe(
+  result,
+  RD.fold3(
+    constant(<div>Loading...</div>),
+    e => <div>Query failed: {e}</div>,
+    result => (
+      <>
+        <h1>Production text</h1>
+        <div>
+          {result.map(row => (
+            <div key={row.id}>
+              <h2>{row.text}</h2>
+              {row.optional && <p>{row.optional}</p>}
+            </div>
+          ))}
+        </div>
+      </>
+    )
+  )
+            }
         </p>
         <p>{process.env.REACT_APP_SUPABASE_URL}</p>
         <a
