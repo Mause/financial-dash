3cad3141e757d6a4ac4439ef98fbf695bd825eac
Elliana May <me@mause.me>
2021-08-09 17:32:10 +0800

    Add missing types

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import useSWR from "swr";
 
 function App() {
   const result = useTable<
-    definitions["Bill"] & { Vendor: definitions["Vendor"] }
+    definitions["Bill"] & { Vendor: definitions["Vendor"], Payment: Array<definitions['Payment'] & {Payer: definitions['Payer']}> }
   >(
     "Bill",
     `
