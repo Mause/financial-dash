af67eec0b9fc22fdd9e1d60d0b3ffede9f215b28
Elliana May <me@mause.me>
2021-08-09 14:58:49 +0800

    Try join

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import * as RD from "@devexperts/remote-data-ts";
 import useSWR from "swr";
 
 function App() {
-  const result = useTable<definitions["Bill"]>("Bill", "*");
+  const result = useTable<definitions["Bill"]>("Bill", `
+    id,
+    amount,
+    vendor (
+      name
+    )`);
+  console.log(result);
   const { data, error } = useSWR("https://launtel.vercel.app/api/transactions");
 
   return (
