3e69dc886ffa63b61f6a23a5d07d9edf983dcc24
Elliana May <me@mause.me>
2021-08-10 15:08:36 +0800

    Fix error message

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
               </>
             ),
             constant(<div>Signing in...</div>),
-            (error) => <div>{error.toString()}</div>,
+            (error) => <div>{error.message == 'Did not return a session.' ? 'Please check your email inbox for a signin link' : error.toString()}</div>,
             () => (
               <button
                 onClick={(e) => {
