e903e605d29f016162b782574b9b005cbc8a99ba
Elliana May <me@mause.me>
2021-08-23 12:08:08 +0800

    Create ping.ts

---
+++ api/ping.ts
@@ -0,0 +1,6 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import * as IN from '../src/invoice-ninja';
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  res.json(IN.paths["/api/v1/clients/create"]);
+}
