8a2f60a203804660162618938f3148891efdf735
Elliana May <me@mause.me>
2021-08-23 13:22:25 +0800

    Add line item

--- api/ping.ts
+++ api/ping.ts
@@ -12,10 +12,17 @@ const axios = Axios.create({
 });
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  const path = "/api/v1/clients/create";
+  const path = "/api/v1/invoices/create";
   type op =
     paths[typeof path]["get"]["responses"][200]["content"]["application/json"];
   const { data } = await axios.get<op>(path);
+  
+  data.line_items.push({
+    cost: 69.81,
+    product_cost: 69.81,
+    product_key: "Internet",
+    quantity: 0.25
+  });
 
   res.json(data);
 }
