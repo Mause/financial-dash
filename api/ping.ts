29fc3348c1fefb70d47c412bf10c7d3926e75cf8
Elliana May <me@mause.me>
2021-08-23 12:41:57 +0800

    Correct import

--- api/ping.ts
+++ api/ping.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { paths } from "../src/invoice-ninja";
-import * as Axios from "axios";
+import Axios from "axios";
 
 const base = "https://api.invoicing.co";
 
@@ -8,7 +8,7 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   const path = "/api/v1/clients/create";
   type op =
     paths[typeof path]["get"]["responses"][200]["content"]["application/json"];
-  const data = await Axios.get<op>(base + path);
+  const { data } = await Axios.get<op>(base + path);
 
   res.json(data);
 }
