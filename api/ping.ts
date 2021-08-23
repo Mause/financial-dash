2463593dda0c9796312e91f2efcc265606da7d3c
Elliana May <me@mause.me>
2021-08-23 12:54:55 +0800

    Secrets

--- api/ping.ts
+++ api/ping.ts
@@ -2,13 +2,20 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { paths } from "../src/invoice-ninja";
 import Axios from "axios";
 
-const base = "https://api.invoicing.co";
+const axios = Axios.create({
+  baseURL: "https://api.invoicing.co",
+  headers: {
+    "X-Api-Secret": process.env.INVOICE_NINJA_SECRET,
+    "X-Api-Token": process.env.INVOICE_NINJA_TOKEN,
+    "X-Requested-With": "XMLHttpRequest",
+  },
+});
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   const path = "/api/v1/clients/create";
   type op =
     paths[typeof path]["get"]["responses"][200]["content"]["application/json"];
-  const { data } = await Axios.get<op>(base + path);
+  const { data } = await axios.get<op>(path);
 
   res.json(data);
 }
