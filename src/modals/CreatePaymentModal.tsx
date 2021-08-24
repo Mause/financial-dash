5809d2d6d4c04b92b4d0c275ae0867e6ff7e5acb
Elliana May <me@mause.me>
2021-08-24 12:44:51 +0800

    Use await

--- src/modals/CreatePaymentModal.tsx
+++ src/modals/CreatePaymentModal.tsx
@@ -25,12 +25,14 @@ export function CreatePaymentModal(props: {
       renderAs="form"
       onSubmit={async (e: FormEvent<any>) => {
         e.preventDefault();
-        Axios.post("/api/ping", {
+        
+        await Axios.post("/api/ping", {
           client_id: RD.isSuccess(payers)
             ? payers.value.find((p) => p.id === payer)!.invoice_ninja_id
             : undefined,
           amount,
-        }).then(console.log.bind(console), console.error.bind(console));
+        });
+
         await createPayment({
           paidBy: payer,
           amount,
