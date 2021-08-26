58452be4c59f715b7ba5b808a2bdbffcd76fba43
Elliana May <me@mause.me>
2021-08-26 15:19:32 +0800

    Create [invoice].ts

---
+++ api/[invoice].ts
@@ -0,0 +1,17 @@
+import { factory } from 'vercel-jwt-auth';
+import invoiceninja from '../support/invoiceninja';
+import {paths} from '../invoiceninja';
+
+const authenticate = factory(process.env.JWT_SECRET!);
+
+export default authenticate(async function(res, req) {
+  const parts = res.url.split('/');
+  const invoice_id = parts[parts.length-1];
+
+  if (req.method === 'GET') {
+    const path = '/api/v1/invoice/{id}';
+    res.json((await invoiceninja.get<paths[typeof path]['get'][200]['application/json']>(path.replace("{id}", invoice_id).data));
+  } else {
+    res.json(`${req.method} not supported`);
+  }
+});
