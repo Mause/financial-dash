4233d7c6c330087b8578a803a9916f089b72a153
Elliana May <me@mause.me>
2021-08-26 16:03:12 +0800

    Fix data return

--- api/[invoice].ts
+++ api/[invoice].ts
@@ -11,9 +11,11 @@ export default authenticate(async function (req, res) {
   if (req.method === "GET") {
     const path = "/api/v1/invoices/{id}";
     res.json(
+      (
       await invoiceninja.get<
         paths[typeof path]["get"]["responses"][200]["content"]["application/json"]
-      >(path.replace("{id}", invoice_id).data)
+      >(path.replace("{id}", invoice_id))
+        ).data
     );
   } else {
     res.json(`${req.method} not supported`);
