12754ab03cf22e7e2ddc3682214199d77c63b728
Elliana <me@mause.me>
2021-08-31 07:59:10 +0800

    Add register payment endpoint

--- api_tests/api.test.ts
+++ api_tests/api.test.ts
@@ -1,47 +1,78 @@
 import { createServer } from "vercel-node-server";
 import listen from "test-listen";
-import axios from "axios";
+import Axios from "axios";
 import { Server } from "net";
 import { sign } from "jsonwebtoken";
 import invoiceninja from "../support/invoiceninja";
 import moxios from "moxios";
 
-let server: Server;
-let url: string;
-
 const SECRET = "SECRET";
-
-beforeAll(async () => {
-  process.env.JWT_SECRET = SECRET;
-  process.env.INVOICE_NINJA_TOKEN = "TOKEN";
-  process.env.INVOICE_NINJA_SECRET = "SECRET";
-  moxios.install(invoiceninja);
-
-  const routeUnderTest = require("../api/[invoice]");
-  server = createServer(routeUnderTest.default);
-  url = await listen(server);
+process.env.JWT_SECRET = SECRET;
+process.env.INVOICE_NINJA_TOKEN = "TOKEN";
+process.env.INVOICE_NINJA_SECRET = "SECRET";
+const axios = Axios.create({
+  headers: {
+    Authorization:
+      "Bearer " +
+      sign(
+        {
+          aud: "authenticated",
+        },
+        SECRET
+      ),
+  },
 });
 
-afterAll(() => {
-  server.close();
-  moxios.uninstall(invoiceninja);
-});
-
-it("should return the expected response", async () => {
+testApi("../api/[invoice]", "GET /invoice/hello", async (url) => {
   moxios.stubOnce("GET", /ident/, {
     response: {},
   });
-  const response = await axios.get(url + "/ident", {
-    headers: {
-      Authorization:
-        "Bearer " +
-        sign(
-          {
-            aud: "authenticated",
-          },
-          SECRET
-        ),
-    },
-  });
+  const response = await axios.get(url + "/ident");
   expect(response.data).toEqual({});
 });
+
+testApi("../api/[invoice]", "PUT /invoice/hello", async (url) => {
+  moxios.stubOnce("PUT", /ident/, {
+    response: {},
+  });
+  const response = await axios.put(url + "/ident", { status: "PAID" });
+  expect(response.data).toEqual({});
+});
+
+testApi("../api/payment", "POST /payment", async (url) => {
+  moxios.stubOnce("POST", /.*/, {
+    response: { payment_id: "payment_id" },
+  });
+  const response = await axios.post(url, {
+    client_id: "client_id",
+    invoice_id: "invoice_id",
+    transaction_reference: "transaction_reference",
+    amount: 1500,
+  });
+  expect(response.data).toEqual({ payment_id: "payment_id" });
+});
+
+function testApi(
+  apiFunction: string,
+  description: string,
+  testFunction: (url: string) => void
+) {
+  describe(description, () => {
+    let server: Server;
+    let url: string;
+
+    beforeAll(async () => {
+      moxios.install(invoiceninja);
+      const routeUnderTest = require(apiFunction);
+      server = createServer(routeUnderTest.default);
+      url = await listen(server);
+    });
+
+    afterAll(() => {
+      server.close();
+      moxios.uninstall(invoiceninja);
+    });
+
+    it(description, () => testFunction(url));
+  });
+}
