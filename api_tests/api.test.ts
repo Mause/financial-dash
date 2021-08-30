0fd720658271858d135f960822b43fe1d6e4faa6
Elliana <me@mause.me>
2021-08-30 17:29:16 +0800

    Implement test in full

--- api_tests/api.test.ts
+++ api_tests/api.test.ts
@@ -1,22 +1,47 @@
 import { createServer } from "vercel-node-server";
 import listen from "test-listen";
 import axios from "axios";
-import routeUnderTest from "../api/invoice";
 import { Server } from "net";
+import { sign } from "jsonwebtoken";
+import invoiceninja from "../support/invoiceninja";
+import moxios from "moxios";
 
 let server: Server;
 let url: string;
 
+const SECRET = "SECRET";
+
 beforeAll(async () => {
-  server = createServer(routeUnderTest);
+  process.env.JWT_SECRET = SECRET;
+  process.env.INVOICE_NINJA_TOKEN = "TOKEN";
+  process.env.INVOICE_NINJA_SECRET = "SECRET";
+  moxios.install(invoiceninja);
+
+  const routeUnderTest = require("../api/[invoice]");
+  server = createServer(routeUnderTest.default);
   url = await listen(server);
 });
 
 afterAll(() => {
   server.close();
+  moxios.uninstall(invoiceninja);
 });
 
 it("should return the expected response", async () => {
-  const response = await axios.get(url, { params: { name: "Pearl" } });
-  expect(response.data).toBe("Hello Pearl");
+  moxios.stubOnce("GET", /ident/, {
+    response: {},
+  });
+  const response = await axios.get(url + "/ident", {
+    headers: {
+      Authorization:
+        "Bearer " +
+        sign(
+          {
+            aud: "authenticated",
+          },
+          SECRET
+        ),
+    },
+  });
+  expect(response.data).toEqual({});
 });
