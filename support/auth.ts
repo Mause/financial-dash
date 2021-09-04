a6971d02bbcb9f6bce2833f2f6648e9188c3c5e8
Elliana <me@mause.me>
2021-09-04 22:44:56 +0800

    Fix errors

--- support/auth.ts
+++ support/auth.ts
@@ -1,12 +1,13 @@
 import { factory } from "vercel-jwt-auth";
 import { VercelRequest, VercelResponse, VercelApiHandler } from "@vercel/node";
 
+const authenticate = factory(process.env.JWT_SECRET!);
+
 export default (handler: VercelApiHandler) => {
-  return (req: VercelRequest, res: VercelResponse) => {
+  return async (req: VercelRequest, res: VercelResponse) => {
     try {
-      return factory(process.env.JWT_SECRET!)(handler)(req, res);
+      await authenticate(handler)(req, res);
     } catch (e) {
-      console.error("Error:", e);
       if (Array.isArray(e)) {
         res.status(422);
         res.json(e);
