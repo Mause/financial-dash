27ad60747f6963d8e1eed3775a41f0ccc5368ee5
Elliana May <me@mause.me>
2021-09-05 01:47:05 +0800

    Validate invoice

--- api/invoice.ts
+++ api/invoice.ts
@@ -3,6 +3,17 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { paths } from "../src/invoice-ninja";
 import invoiceninja from "../support/invoiceninja";
 import { factory } from "vercel-jwt-auth";
+import { validateOrReject, IsNotEmpty } from "class-validator";
+
+class PostInvoice {
+  @IsNotEmpty()
+  client_id!: string;
+  @IsNotEmpty()
+  amount!: number;
+  constructor(body: any) {
+    Object.assign(this, body);
+  }
+}
 
 const authenticate = factory(process.env.JWT_SECRET!);
 
@@ -10,13 +21,16 @@ export default authenticate(async function (
   req: VercelRequest,
   res: VercelResponse
 ) {
+  const request = new PostInvoice(req.body);
+  await validateOrReject(request);
+
   const path = "/api/v1/invoices/create";
   type op =
     paths[typeof path]["get"]["responses"][200]["content"]["application/json"];
   let { data } = await invoiceninja.get<op>(path);
 
-  data.client_id = req.body.client_id;
-  const cost = Number(req.body.amount) / 100;
+  data.client_id = request.client_id;
+  const cost = request.amount / 100;
 
   data.date = new Date().toISOString();
 
