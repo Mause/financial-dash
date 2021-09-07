9a88f25a69bb59d842a7378ee36cdeb920b08a19
Elliana May <me@mause.me>
2021-09-07 10:25:15 +0800

    Create class_test.ts

---
+++ api/class_test.ts
@@ -0,0 +1,9 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+class ClassTest {
+invoke(req: VercelRequest, res: VercelResponse) {
+  res.status(200).send('hello');
+}
+}
+
+export default (res, req) => new ClassTest().invoke(req, res);
