0f4a2058239a4b3f740b314666a00845d25c769c
Elliana May <me@mause.me>
2021-09-15 07:08:45 +0000

    Try shape instead

--- src/use_api.ts
+++ src/use_api.ts
@@ -2,12 +2,11 @@ import * as O from "fp-ts/Option";
 import { constant } from "fp-ts/lib/function";
 import { useToken } from "./auth";
 import { Configuration } from "./financial-dash";
-import { BaseAPI } from "./financial-dash/base";
 
-export default function useApi<T extends BaseAPI>(clazz: NewableFunction): T {
+export default function useApi<T>(clazz: { new (c: Configuration): T }): T {
   const token = useToken();
 
-  return clazz(
+  return new clazz(
     new Configuration({
       accessToken() {
         return O.getOrElse(constant(""))(token);
