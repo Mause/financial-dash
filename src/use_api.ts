e5025a5678fea9eefdb18096551c94b018663f90
Elliana May <me@mause.me>
2021-09-15 14:25:04 +0800

    Fix imports

--- src/use_api.ts
+++ src/use_api.ts
@@ -1,8 +1,8 @@
 import { useState, ChangeEvent, FormEvent } from "react";
 import * as O from "fp-ts/Option";
 import { constant } from "fp-ts/lib/function";
-import { useToken } from "../auth";
-import { InvoiceApi, Configuration } from "../financial-dash";
+import { useToken } from "./auth";
+import { InvoiceApi, Configuration } from "./financial-dash";
 
 export default function useApi<T>(clazz: (c: Configuration) => T): T {
   const token = useToken();
