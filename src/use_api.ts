39e636744cd8ab3290396807e18c65cd8fc282ce
Elliana May <me@mause.me>
2021-09-15 06:50:59 +0000

    Refactor useApi to use useEffect

--- src/use_api.ts
+++ src/use_api.ts
@@ -1,17 +1,32 @@
-import { useState, ChangeEvent, FormEvent } from "react";
 import * as O from "fp-ts/Option";
 import { constant } from "fp-ts/lib/function";
 import { useToken } from "./auth";
-import { InvoiceApi, Configuration } from "./financial-dash";
+import { Configuration } from "./financial-dash";
+import { useEffect, useState } from "react";
 
 export default function useApi<T>(clazz: (c: Configuration) => T): T {
   const token = useToken();
-
-  return clazz(
-    new Configuration({
-      accessToken() {
-        return O.getOrElse(constant(""))(token);
-      },
-    })
+  const [api, setApi] = useState(
+    clazz(
+      new Configuration({
+        accessToken() {
+          throw new Error("Don't have access token yet");
+        },
+      })
+    )
   );
+
+  useEffect(() => {
+    setApi(
+      clazz(
+        new Configuration({
+          accessToken() {
+            return O.getOrElse(constant(""))(token);
+          },
+        })
+      )
+    );
+  }, [token]);
+
+  return api;
 }
