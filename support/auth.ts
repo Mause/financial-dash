d0570b3752e287a2b5bba06854ef3358a34ba2f9
Elliana May <me@mause.me>
2021-09-19 11:44:17 +0800

    Logtail?

--- support/auth.ts
+++ support/auth.ts
@@ -2,8 +2,24 @@ import { factory } from "vercel-jwt-auth";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import * as Sentry from "@sentry/node";
 import pino from "pino";
+import { Logtail } from "@logtail/node";
 
-const log = pino({ prettyPrint: true });
+const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
+
+import build from "pino-abstract-transport";
+
+const transport = build((source) =>
+  source.on("data", (data) => logtail.error(data))
+);
+
+const log = pino({
+  prettyPrint: true,
+  serializers: {
+    what: (value) => {
+      logtail.info(value);
+    },
+  },
+});
 const authenticate = factory(process.env.JWT_SECRET!);
 
 type VercelApiHandler = (
