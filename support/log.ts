d1412b74908912fb39db6c3cc48bdec3f5f9b463
Elliana May <me@mause.me>
2021-09-20 05:30:21 +0000

    Add sanity logs

--- support/log.ts
+++ support/log.ts
@@ -5,16 +5,15 @@ import bunyan from "bunyan";
 const streams: bunyan.Stream[] = [{ stream: process.stdout }];
 
 if (process.env.LOGTAIL_TOKEN) {
-  streams.push({
-    stream: new LogtailStream(
-      new Logtail(process.env.LOGTAIL_TOKEN, {
-        batchSize: 1,
-        batchInterval: 1,
-        ignoreExceptions: false,
-        syncMax: 1,
-      })
-    ),
+  console.log("adding logtail stream");
+  const logtail = new Logtail(process.env.LOGTAIL_TOKEN, {
+    batchSize: 1,
+    batchInterval: 1,
+    ignoreExceptions: false,
+    syncMax: 1,
   });
+  logtail.info("Starting up...");
+  streams.push({ stream: new LogtailStream(logtail), reemitErrorEvents: true });
 }
 
 export const log = bunyan.createLogger({
