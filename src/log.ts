afd6e39227a9a4276cf2dc07a4950959df8a61e0
Elliana May <me@mause.me>
2021-09-21 05:31:52 +0000

    Use browser-bunyan

--- src/log.ts
+++ src/log.ts
@@ -1,35 +1,19 @@
 import { Logtail } from "@logtail/browser";
 import { Logtail as Node } from "@logtail/node";
 import { LogtailStream } from "@logtail/bunyan";
-import bunyan, { createLogger } from "bunyan";
-import { Writable } from "stream";
+import {
+  createLogger,
+  LogStream,
+  ConsoleFormattedStream,
+} from "browser-bunyan";
 
-interface LogEvent {
-  time: Date;
-  level: number;
-  msg: string;
-}
-
-class ConsoleStream extends Writable {
-  write(record: string | Uint8Array): boolean {
-    const rec = JSON.parse(record.toString()) as LogEvent;
-    console.log(
-      "[%s] %s: %s",
-      rec.time.toISOString(),
-      bunyan.nameFromLevel[rec.level],
-      rec.msg
-    );
-    return true;
-  }
-}
-
-const streams: bunyan.Stream[] = [{ stream: new ConsoleStream() }];
+const streams: { stream: LogStream }[] = [
+  { stream: new ConsoleFormattedStream() },
+];
 
 if (process.env.REACT_APP_LOGTAIL_SOURCE_TOKEN) {
   const logtail = new Logtail(process.env.REACT_APP_LOGTAIL_SOURCE_TOKEN);
-  streams.push({
-    stream: new LogtailStream(logtail as unknown as Node),
-  });
+  streams.push({ stream: new LogtailStream(logtail as unknown as Node) });
 }
 
 export default createLogger({ name: "financial-dash-app", streams });
