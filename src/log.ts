85fe316a0bc2c8d5b605a81e48386811edcee5ad
Elliana May <me@mause.me>
2022-03-13 04:07:15 +0000

    chore: Inline getLogLevel

--- src/log.ts
+++ src/log.ts
@@ -1,15 +1,56 @@
 import { Logtail } from "@logtail/browser";
-import { getLogLevel } from "@logtail/bunyan/dist/es6/helpers";
 import {
   createLogger,
   LogStream,
   ConsoleFormattedStream,
 } from "browser-bunyan";
-import { Context } from "@logtail/types";
+import { Context, LogLevel } from "@logtail/types";
 
 const streams: { stream: LogStream }[] = [
   { stream: new ConsoleFormattedStream() },
 ];
+
+export function getLogLevel(level: string | number) {
+  // Are we dealing with a string log level?
+  if (typeof level === "string") {
+    switch (level.toLowerCase()) {
+      // Trace is ignored
+      case "trace":
+        throw new Error();
+      // Error
+      case "fatal":
+      case "error":
+        return LogLevel.Error;
+      // Warn
+      case "warn":
+        return LogLevel.Warn;
+      // Debug
+      case "debug":
+        return LogLevel.Debug;
+    }
+  } else if (typeof level === "number") {
+    // If level <=, consider it 'tracing' and move on
+    if (level <= 10) {
+      throw new Error();
+    }
+    // Debug
+    if (level <= 20) {
+      return LogLevel.Debug;
+    }
+    // Info
+    if (level <= 30) {
+      return LogLevel.Info;
+    }
+    // Warn
+    if (level <= 40) {
+      return LogLevel.Warn;
+    }
+    // Everything above this level is considered an error
+    return LogLevel.Error;
+  }
+  // By default, return info
+  return LogLevel.Info;
+}
 
 export class LogtailStream {
   constructor(private _logtail: Logtail) {}
