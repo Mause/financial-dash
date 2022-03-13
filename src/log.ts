3a283aa92c929c3e8b192df5efae877e3926996f
Elliana May <me@mause.me>
2022-03-13 03:45:18 +0000

    refactor: Remove @logtail/bunyan usage in browser

--- src/log.ts
+++ src/log.ts
@@ -1,29 +1,52 @@
 import { Logtail } from "@logtail/browser";
-import { Logtail as Node } from "@logtail/node";
-import { LogtailStream } from "@logtail/bunyan";
+import { getLogLevel } from "@logtail/bunyan/dist/es6/helpers";
 import {
   createLogger,
   LogStream,
   ConsoleFormattedStream,
 } from "browser-bunyan";
+import { Context } from "@logtail/types";
 
 const streams: { stream: LogStream }[] = [
   { stream: new ConsoleFormattedStream() },
 ];
 
-class XLogtailStream extends LogtailStream {
-  constructor(logtail: Logtail) {
-    super(logtail as unknown as Node);
-  }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  write(chunk: any): boolean {
-    return super.write(JSON.stringify(chunk));
+export class LogtailStream {
+  constructor(private _logtail: Logtail) {}
+
+  write(log: any) {
+    // Log should have string `msg` key, > 0 length
+    if (typeof log.msg !== "string" || !log.msg.length) {
+      return;
+    }
+    // Logging meta data
+    const meta: Context = {};
+    // Copy `time` if set
+    if (typeof log.time === "string" || log.time.length) {
+      const time = new Date(log.time);
+      if (!isNaN(time.valueOf())) {
+        meta.dt = time;
+      }
+    }
+    // Carry over any additional data fields
+    Object.keys(log)
+      .filter((key) => ["time", "msg", "level", "v"].indexOf(key) < 0)
+      .forEach((key) => (meta[key] = log[key]));
+    // Determine the log level
+    let level;
+    try {
+      level = getLogLevel(log.level);
+    } catch (_) {
+      return;
+    }
+    // Log to Logtail
+    void this._logtail.log(log.msg, level, meta);
   }
 }
 
 if (process.env.REACT_APP_LOGTAIL_SOURCE_TOKEN) {
   const logtail = new Logtail(process.env.REACT_APP_LOGTAIL_SOURCE_TOKEN);
-  const stream = new XLogtailStream(logtail);
+  const stream = new LogtailStream(logtail);
   streams.push({ stream });
 }
 
