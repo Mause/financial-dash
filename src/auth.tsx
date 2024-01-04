83086f498c7b8a7ff0ad64748e149492af611ebb
Elliana May <me@mause.me>
2024-01-04 14:18:17 +0800

    Revert "fix(deps): update dependency @supabase/supabase-js to v2"

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,22 +1,17 @@
-import { fold, fromNullable, isSome, Option } from "fp-ts/Option";
+import { fold, fromNullable, some, Option, chain } from "fp-ts/Option";
 import { constant, pipe } from "fp-ts/function";
 import { useSupabase } from "react-supabase-fp";
 import { SWRConfig } from "swr";
-import React, { ReactNode, ReactPortal, useEffect, useState } from "react";
+import React, { ReactNode, ReactPortal } from "react";
 
 export function useToken(): Option<string> {
   const supabase = useSupabase();
-  const [session, setSession] = useState<string>();
 
-  useEffect(() => {
-    if (isSome(supabase)) {
-      supabase.value.auth.getSession().then(({ data }) => {
-        setSession(data.session?.access_token);
-      });
-    }
-  }, []);
-
-  return fromNullable(session);
+  return pipe(
+    supabase,
+    chain((thing) => fromNullable(thing.auth.session())),
+    chain((session) => some(session.access_token)),
+  );
 }
 function useFetcher() {
   const token = useToken();
