04d87e88d2fe55610ae5c5a6047857b779810ee0
Elliana May <me@mause.me>
2024-01-04 14:04:48 +0800

    tweak useToken

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,17 +1,22 @@
-import { fold, fromNullable, some, Option, chain } from "fp-ts/Option";
+import { fold, fromNullable, isSome, Option } from "fp-ts/Option";
 import { constant, pipe } from "fp-ts/function";
 import { useSupabase } from "react-supabase-fp";
 import { SWRConfig } from "swr";
-import React, { ReactNode, ReactPortal } from "react";
+import React, { ReactNode, ReactPortal, useEffect, useState } from "react";
 
 export function useToken(): Option<string> {
   const supabase = useSupabase();
+  const [session, setSession] = useState<string>();
 
-  return pipe(
-    supabase,
-    chain((thing) => fromNullable(thing.auth.session())),
-    chain((session) => some(session.access_token)),
-  );
+  useEffect(() => {
+    if (isSome(supabase)) {
+      supabase.value.auth.getSession().then(({ data }) => {
+        setSession(data.session?.access_token);
+      });
+    }
+  }, []);
+
+  return fromNullable(session);
 }
 function useFetcher() {
   const token = useToken();
