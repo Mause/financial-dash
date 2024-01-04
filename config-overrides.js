3d10c218f6c9b68ee828ea381d3de365ef2f6c3e
Elliana May <me@mause.me>
2024-01-04 13:36:08 +0800

    rewired

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { override, babelInclude } = require("customize-cra");
+
+module.exports = function (config, env) {
+  return Object.assign(
+    config,
+    override(
+      babelInclude([
+        path.resolve("src"),
+        path.resolve("packages"),
+        path.resolve("support"),
+        path.resolve("api"),
+      ]),
+    )(config, env),
+  );
+};
